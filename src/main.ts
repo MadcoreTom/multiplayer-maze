1990bf200a55fcbdffb8bfc24a95f6c1c6e51519
MadcoreTom <madcoretom@gmail.com>
2023-11-03 18:30:03 +1100

    Basic rendering

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,26 @@ import { Arr } from "./arr";
 
 console.log("Maze");
 
-const arr = new Arr<string>(9, 9, ".");
+const arr = new Arr<string>(19, 19, ".");
 arr.init((x, y) =>
-    x % 2 == 0 && y % 2 == 0 ? "#" : "."
+    x % 2 == 1 && y % 2 == 1 ? "#" : "."
 );
 
 console.log("Done")
+
+
+const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+
+function render() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 1000, 1000);
+    const size = 20;
+    arr.forEach((x, y, v) => {
+        ctx.fillStyle = v == "#" ? "orangered" : "white";
+        ctx.fillRect(x * size, y * size, size, size);
+    });
+}
+
+render();
