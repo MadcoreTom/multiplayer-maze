89b5cb1614730b8b30d7e714b9f9bf06cd800c05
MadcoreTom <madcoretom@gmail.com>
2023-11-05 14:14:49 +1100

    The maze view scrolls

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { Path, solve } from "./astar";
 import { maze } from "./maze";
 
 const STEPS_PER_FRAME = 10;
-const MAX_DISTANCE = 15;
-const MAZE_SIZE = 24;
+const MAX_DISTANCE = 18;
+const MAZE_SIZE = 40;
 const SCALE = Math.floor(1000 / MAZE_SIZE);
 
 const arr = new Arr<string>(MAZE_SIZE, MAZE_SIZE, ".");
@@ -19,27 +19,46 @@ const mazeGenerator = maze(arr, ".", "#", MAX_DISTANCE);
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
+let offset:[number,number] = [0,0];
 
 
 function render(path: Path | null) {
-    ctx.fillStyle = "orangered";
+    ctx.fillStyle = "grey";
     ctx.fillRect(0, 0, 1000, 1000);
     arr.forEach((x, y, v) => {
         ctx.fillStyle = v == "#" ? "white" : "black";
-        ctx.fillRect(x * SCALE, y * SCALE, SCALE, SCALE);
+        x = (x + arr.width - offset[0]) %arr.width;
+        y = (y + arr.height - offset[1]) %arr.height;
+        ctx.fillRect(Math.floor(x * SCALE),Math.floor( y * SCALE), SCALE, SCALE);
     });
 
     if (path) {
         ctx.fillStyle = "blue";
         path.forEach(([x, y]) => {
+            x = (x + arr.width - offset[0]) %arr.width;
+            y = (y + arr.height - offset[1]) %arr.height;
             ctx.fillRect(x * SCALE + SCALE / 4, y * SCALE + SCALE / 4, SCALE / 2, SCALE / 2);
         })
     }
 }
 
+let lastTime = 0;
+const MAX_FRAME_TIME = 1000/10;
 
 
 function tick(time: number) {
+    const delta = Math.min(MAX_FRAME_TIME, time - lastTime);
+    lastTime = time;
+
+    offset[0] += delta / 300;
+    offset[1] += delta / 200;
+    if(offset[0] > arr.width){
+        offset[0] -= arr.width;
+    }
+    if(offset[1] > arr.height){
+        offset[1] -= arr.height;
+    }
+
     let result;
     for (let i = 0; i < STEPS_PER_FRAME && (!result || !result.done); i++) {
         result = mazeGenerator.next();
@@ -48,9 +67,9 @@ function tick(time: number) {
     console.log("-")
 
     render(path);
-    if (!result.done) {
+    // if (!result.done) {
         window.requestAnimationFrame(tick);
-    }
+    // }
 }
 
 tick(0);
